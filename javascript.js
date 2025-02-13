c164439b9a58189368663896d1f41d084f43ea57
Marcosmz12 <mmolzap0412@g.educaand.es>
2025-02-13 16:15:47 +0100

    cAMBIOS DE JS

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-const listanavegador = document.querySelector('.seccion-pokemons');
+const seccionpokemons = document.querySelector('.seccion-pokemons');
 let URL = "https://pokeapi.co/api/v2/pokemon/";
 
 for(let i = 1; i <= 151; i++){
@@ -10,16 +10,23 @@ for(let i = 1; i <= 151; i++){
 
 function mostrarPokemon(pokemon){
     const divPokemon = document.createElement('div');
-    div.classList.add('pokemon');
-    div.innerHTML = `<img src="" alt="">
-                        <p></p>
-                        <p>r</p>
-                        <div class="tipo-pokemon">
-                            <span id = "planta">Planta</span>
-                            <span id = "veneno">Veneno</span>
-                            </div>
-                        </div>`;
+    const imgPOkemon = document.createElement('img');
+    const numeroPokemon = document.createElement('p');
+    const nombrePokemon = document.createElement('p');
+    const divTipoPokemon = document.createElement('div');
+    const tipoPokemon = document.createElement('span');
     
-    listanavegador.appendChild(divPokemon);
+    div.classList.add('pokemon');
+    numeroPokemon.textContent = `#${pokemon.id}`;
+    nombrePokemon.textContent = pokemon.name;
+    imgPOkemon.src = pokemon.sprites.front_default;
+    divTipoPokemon.classList.add('tipo-pokemon');
+    tipoPokemon.textContent = pokemon.types[0].type.name;
+    if(pokemon.types.length > 1){
+        const tipoPokemon2 = document.createElement('span');
+        tipoPokemon2.textContent = pokemon.types[1].type.name;
+        divTipoPokemon.appendChild(tipoPokemon);
+    }
+    seccionpokemons.appendChild(divPokemon);
     console.log(pokemon);
 }
